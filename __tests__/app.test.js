2e32e1f00bde1a0c583d8e8a26a72fb285122749
g-kimani <77020194+g-kimani@users.noreply.github.com>
2023-05-09 12:30:36 +0100

    3.5 added base endpoint for api info

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,21 @@
 const request = require("supertest");
 const app = require("../app.js");
 const connection = require("../db/connection.js");
+const apiEndpoints = require("../endpoints.json");
 
 afterAll(() => connection.end());
+
+describe("/api", () => {
+  test("GET - status: 200 - responds with object detailing every api endpoint", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then((response) => {
+        const { endpoints } = response.body;
+        expect(endpoints).toEqual(apiEndpoints);
+      });
+  });
+});
 
 describe("/api/topics", () => {
   test("GET - status: 200 - responds with array of topic objects", () => {
--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const { getTopics } = require("./controllers/topics.controllers");
+const { getTopics } = require("./controllers/topics.controller.js");
+const { getApiInfo } = require("./controllers/api.controller.js");
 
 const app = express();
+
+app.get("/api", getApiInfo);
 
 app.get("/api/topics", getTopics);
 
