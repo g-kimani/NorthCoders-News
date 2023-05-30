ed32142aa7aa73bc8e06ff5898dc37701bb8228f
g-kimani <77020194+g-kimani@users.noreply.github.com>
2023-05-30 09:39:59 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const apiRouter = require("./routes/api-router.js");
 const { customErrors, psqlErrors, logError } = require("./error-handlers.js");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
