591b43eb2cd4499e7b114fc00ca1650999a08ad7
g-kimani <77020194+g-kimani@users.noreply.github.com>
2023-07-04 01:13:07 +0100

    redirect to frontend for route

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// TODO - redirects to frontend (messed up cv link)
+app.get("/", (req, res) => {
+  const frontendLink = "https://visionary-kashata-762e78.netlify.app/";
+  res.redirect(frontendLink);
+});
+
 app.use("/api", apiRouter);
 
 app.use((err, req, res, next) => {
